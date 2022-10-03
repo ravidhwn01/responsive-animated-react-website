18a3bdad890b7222f069e916c1c4c38ac553d5c7
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-03 09:18:35 +0530

    install some dependencies

--- responsive/src/App.js
+++ responsive/src/App.js
@@ -1,15 +1,36 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import Services from "./components/Services";
-
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 function App() {
   return (
     <>
-      <Home/>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
+
+      {/* <Home/>
       <Services/>
-      <About/>
+      <About/> */}
     </>
   );
 }
---
+++ responsive/src/components/Error.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Error() {
+  return (
+    <div>Oops! page not found</div>
+  )
+}
