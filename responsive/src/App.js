5b2b68a1b70ec4c1303b7b9092f57a1fa5eebb7b
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-05 16:17:02 +0530

    added Link in navbar

--- responsive/src/App.js
+++ responsive/src/App.js
@@ -1,13 +1,16 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import About from "./components/About";
 import Home from "./components/Home";
 import Services from "./components/Services";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Contact from "./components/Contact";
-import Error from "./components/Error";
+// import Error from "./components/Error";
+import Navbar from "./components/Navbar";
 
 export default function App(){
   return (
     <>
+    <Navbar/>
       <Router>
         <Switch>
           <Route exact path="/">
@@ -31,5 +34,6 @@ export default function App(){
     </>
   );
 }
+
 
 
