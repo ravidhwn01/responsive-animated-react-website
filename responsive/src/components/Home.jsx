dc629977d1d04568d593dffa6f748a7ce48f641b
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-06 18:14:36 +0530

    edding img in home component

--- responsive/src/components/Home.jsx
+++ responsive/src/components/Home.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
     <>
-      <div className="container-fluid">
-        
-      </div>
+      <section id="header" className="">
+        <div className="container">
+          <div className="row">
+            <div className="col-10-mx-auto">
+            <div className="row">
+
+            
+              <div className="col-md-6 pt-5 ">
+                <h1>
+                  grow your busines with <strong>ravi dhawan</strong>
+                </h1>
+                <h2 className="mt-3">
+                  we are here to improve your skills and confidence
+                </h2>
+                <div className="mt-3">
+                  <a href="">get started</a>
+                </div>
+              </div>  
+              <div className="col-md-6 pt-5 " >
+                <img src="https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg" alt="" />
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
